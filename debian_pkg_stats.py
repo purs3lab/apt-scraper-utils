fbbc1b4a6ca6f59080237754d95d4abd4db5c344
Aravind Machiry <amachiry@purdue.edu>
2023-10-20 16:31:03 -0400

    adding

---
+++ debian_pkg_stats.py
@@ -0,0 +1,23 @@
+import json
+
+class DebianPkgStats:
+    def __init__(self, pkg_name) -> None:
+        self.pkg_name = pkg_name
+    
+    def add_dependency_status(self, status: str):
+        self.dependency_status = status
+    
+    def add_download_status(self, status:str):
+        self.download_status = status
+    
+    def add_extract_status(self, status:str):
+        self.extract_status = status
+    
+    def add_build_status(self, status:str):
+        self.build_status = status
+    
+    def add_codeql_status(self, status:str):
+        self.codeql_status = status
+    
+    def toJson(self) -> str:
+        return json.dumps(self, default=lambda o: o.__dict__)
--- extract_bc_sources.py
+++ extract_bc_sources.py
@@ -9,24 +9,37 @@
 ##########################################################################
 from pkg_manager import PackageManager
 from ctypes.util import find_library
+from debian_pkg_stats import DebianPkgStats
 import os
 import subprocess
 import sys
+import json
 import re
 
-source_file_to_read_packages_from = "/home/singhav/Sources"
+source_file_to_read_packages_from = "/home/machiry/Downloads/debiancodeql/Sources"
 mirror_url = "http://mirror.math.ucdavis.edu/ubuntu/"
-local_download_folder_for_sources = "/home/singhav/apt_scraper_sources"
-extracted_tar_sources = '/home/singhav/extracted_tar_sources'
-afl_fuzzing_sources = '/home/singhav/afl_sources'
+local_download_folder_for_sources = "/home/machiry/Downloads/debiancodeql/downloadsources"
+extracted_tar_sources = '/home/machiry/Downloads/debiancodeql/extracted_tar_sources'
+
+def create_folder(dirn: str):
+    # create folder if not exists
+    if not os.path.isdir(dirn):
+        os.makedirs(dirn)
+
+def path_to_folder(currd: str):
+    # get path to only folder in this directory
+    for cf in os.listdir(currd):
+        if os.path.isdir(os.path.join(currd, cf)):
+            return os.path.join(currd, cf)
+    return None
 
 if not os.path.isdir(local_download_folder_for_sources):
     cmd = "(" + "mkdir " + local_download_folder_for_sources + ")"
     subprocess.call(cmd, shell=True)
 
-if not os.path.isdir(afl_fuzzing_sources):
-    cmd = "(" + "mkdir " + afl_fuzzing_sources + ")"
-    subprocess.call(cmd, shell=True)
+#if not os.path.isdir(afl_fuzzing_sources):
+#    cmd = "(" + "mkdir " + afl_fuzzing_sources + ")"
+#    subprocess.call(cmd, shell=True)
 
 if not os.path.isdir(extracted_tar_sources):
     cmd = "(" + "mkdir " + extracted_tar_sources + ")"
@@ -46,58 +59,73 @@ packages_available = p.all_pkg_entries
 cmd = "(" + "export DEBIAN_FRONTEND=noninteractive" + ")"
 subprocess.call(cmd, shell=True)
 
+all_pkg_status = {}
+
+num = 0
+max_num_pkgs = 1
 for pkgs in packages_available:
-    
+    if num >= max_num_pkgs:
+        break
+    num = num + 1
+    all_pkg_status[pkgs] = DebianPkgStats(pkgs)
+    cstatus = all_pkg_status[pkgs]
     reverse_dependencies = []
     dependency_list = p.dependency_map[pkgs]
+    all_dependencies = []
     for dependencies in dependency_list:
+        all_dependencies.append(dependencies)
         # install all reverse dependencies might be too slow (not necessary), uncomment if needed
         #for reverse_deps in p.reverse_dependency_map[dependencies]:
         #    subprocess.call(['sudo apt -yq install', str(reverse_deps)], shell=True)        
-        subprocess.call(['sudo apt -yq install', str(dependencies)], shell=True)
+        #subprocess.call(['echo machiry_1337 | sudo -S apt -yq install', str(dependencies)], shell=True)
         reverse_dependencies.extend(p.reverse_dependency_map[dependencies])
-    
-    for lib in reverse_dependencies:
-        if (find_library(lib) != None):
-            print()
-            print("...")
-            print("DOWNLOADING "+ str(pkgs) + "from the mirror...")
-            print("...")
-            p.download_package_source(pkgs, local_download_folder_for_sources)
-            break
+    ret = subprocess.call(['echo machiry_1337 | sudo -S apt -yq install', " ".join(all_dependencies)], shell=True)
+    if ret != 0:
+        cstatus.add_dependency_status("FAILED")
+    else:
+        cstatus.add_dependency_status("SUCCESS")
+    print()
+    print("...")
+    print("DOWNLOADING "+ str(pkgs) + "from the mirror...")
+    print("...")
+    pkg_folder = os.path.join(local_download_folder_for_sources, pkgs)
+    p.download_package_source(pkgs, pkg_folder)
+    cstatus.add_download_status("SUCCESS")
 
 
-#Extract the tar sources, build them and install and put them in another folder
-for subdir, dirs, files in os.walk(local_download_folder_for_sources):
-    
-    for File in files:
-        if "orig" in str(File):
-
-            #extract the archive
-            cmd = "(" + "cd " + local_download_folder_for_sources + " && " + "tar -xf " + str(File) + ")"
-            subprocess.call(cmd, shell=True)
-
-            archive_parts = File.split(".orig")
-            underscore_split = archive_parts[0].split("_")
-            directory_name = str(underscore_split[0]) + "-" + str(underscore_split[1])
-
-            configure_path = local_download_folder_for_sources + "/" + directory_name + "/" + "configure"
+for curr_dir in os.listdir(local_download_folder_for_sources):
+    cdir_fullpath = os.path.join(local_download_folder_for_sources, curr_dir)
+    cstatus = all_pkg_status[curr_dir]
+    if os.path.isdir(cdir_fullpath):
+        for tfi in os.listdir(cdir_fullpath):
+            if tfi.endswith(".orig.tar.gz"):
+                #extract the archive
+                print("Extracting " + tfi + "...")
+                cmd = "(" + "cd " + cdir_fullpath + " && " + "tar -xf " + str(tfi) + ")"
+                ret = subprocess.call(cmd, shell=True)
+                if ret != 0:
+                    cstatus.add_extract_status("FAILED")
+                else:
+                    cstatus.add_extract_status("SUCCESS")
+                extract_dir_name = path_to_folder(cdir_fullpath)
+                if extract_dir_name is not None:
+                    configure_path = extract_dir_name + "/" + "configure"
             
-            #check if a configure script exists in the directory
-            if( os.path.exists(configure_path) == True ):
-                print(File)
-                #now cd into the archive
-                cmd = "(" + "cd " + local_download_folder_for_sources + "/" + directory_name + " && " + "export LLVM_COMPILER=clang" + " && " + "CC=wllvm ./configure" +  " && " + "make" + " && " + "make install DESTDIR=" + extracted_tar_sources + "/" + directory_name  + ")"
-                subprocess.call(cmd, shell=True)
+                    #check if a configure script exists in the directory
+                    if( os.path.exists(configure_path) == True ):
+                        #now cd into the archive
+                        cmd = "(" + "cd " + extract_dir_name + " && ./configure" +  " && " + "make" + ")"
+                        ret = subprocess.call(cmd, shell=True)
+                        if ret != 0:
+                            cstatus.add_build_status("FAILED")
+                        else:
+                            cstatus.add_build_status("SUCCESS")
+                    else:
+                        cstatus.add_build_status("FAILED")
 
-#run extract-bc to get the bit-code files form the binaries
-for subdir, dirs, files in os.walk(extracted_tar_sources):
-
-    if subdir.endswith("/bin"):
-
-        for File in files:
-            cmd = "(" + "cd " + subdir + " && " + "extract-bc " + File + " && " + "mv " + File + ".bc " + afl_fuzzing_sources + ")"
-            subprocess.call(cmd, shell=True)
-
-
-
+fp = open("all_pkg_status.json", "w")
+json_strs = {}
+for p in all_pkg_status:
+    json_strs[p] = all_pkg_status[p].toJson()
+fp.write(json.dumps(json_strs))
+fp.close()
